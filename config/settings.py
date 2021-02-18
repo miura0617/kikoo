73afbe2f3fa234e641d80f9a180985f852f269f6
miura0617 <j.miura0617@gmail.com>
2021-02-18 22:19:06 +0900

    SECRET_KEY変更

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eqsh7g1!ybn9rqud8pr-#45cmx$6a7u^s7=wh57+t--x119#53'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
